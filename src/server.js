6bf286bd9e3de4bd4c333d10d8ee4ca23238eb68
Youssef <youssefswailem@gmail.com>
2017-04-12 23:20:30 +0200

    remove auto-updater

--- src/server.js
+++ src/server.js
@@ -1,38 +1,9 @@
 var http = require('http');
-var AutoUpdater = require('auto-updater');
 
-module.exports.run = function(){
+var server = http.createServer();
 
-    var autoupdater = new AutoUpdater({
-        pathToJson: '',
-        autoupdate: true,
-        checkgit: true,
-        jsonhost: 'https://raw.githubusercontent.com/Swailem95/PinkFloydBot/master/package.json',
-        contenthost: 'https://github.com/Swailem95/PinkFloydBot/archive/master.zip',
-        progressDebounce: 0,
-        devmode: false
+  server.on('listening',function(){
+      console.log('Server is running');
     });
 
-    autoupdater.on('check.out-dated', function(v_old, v) {
-        console.warn("Your version is outdated. " + v_old + " of " + v);
-        autoupdater.fire('download-update');
-    });
-
-
-    var server = http.createServer();
-
-    server.on('listening',function(){
-        console.log('Server is running');
-    });
-
-    server.listen(process.env.PORT || 5050);
-
-    autoupdater.fire('check');
-
-    //to keep the bot awake 
-
-    // setInterval(function(){
-    //     http.get("http://pinkfloydbot.herokuapp.com")
-    // }, 300000)
-
-}
+server.listen(process.env.PORT || 5050);
